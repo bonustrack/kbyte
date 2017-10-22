2322903349a28bad3888c4aab8bb485d1c6d3ba2
adcpm <fabien@bonustrack.co>
2017-10-22 23:15:00 +0700

    Init

--- test/test.js
+++ test/test.js
@@ -1,53 +1,33 @@
-const assert = require('assert');
-const howLongTillLunch = require('..');
+// const assert = require('assert');
+const kbyte = require('..');
 
-function MockDate () {
-	this.date = 0;
-	this.hours = 0;
-	this.minutes = 0;
-	this.seconds = 0;
-	this.milliseconds = 0;
+const client = new kbyte.Client('wss://byteball.org/bb');
+
+const test = async () => {
+  /** Get Witnesses */
+  const witnesses = await client.send('get_witnesses', null);
+  console.log('Witnesses', witnesses);
+
+  /** Get Parents */
+  const parents = await client.send('light/get_parents_and_last_ball_and_witness_list_unit', {
+    witnesses,
+  });
+  console.log('Parents', parents);
+
+  /** Get Peers */
+  const peers = await client.send('get_peers', null);
+  console.log('Peers', peers);
+
+  /** Get History */
+  const history = await client.send('light/get_history', {
+    witnesses,
+    requested_joints: [parents.last_stable_mc_ball_unit]
+  });
+  console.log('History', history);
+
+  /** Get Joint */
+  const joint = await client.send('get_joint', parents.last_stable_mc_ball_unit);
+  console.log('Joint', joint);
 };
 
-Object.assign(MockDate.prototype, {
-	getDate () { return this.date; },
-	setDate (date) { this.date = date; },
-	setHours (h) { this.hours = h; },
-	setMinutes (m) { this.minutes = m; },
-	setSeconds (s) { this.seconds = s; },
-	setMilliseconds (ms) { this.milliseconds = ms; },
-	valueOf () {
-		return (
-			this.milliseconds +
-			this.seconds * 1e3 +
-			this.minutes * 1e3 * 60 +
-			this.hours * 1e3 * 60 * 60 +
-			this.date * 1e3 * 60 * 60 * 24
-		);
-	}
-});
-
-const now = new MockDate();
-MockDate.now = () => now.valueOf();
-
-global.Date = MockDate;
-
-function test(hours, minutes, seconds, expected) {
-	now.setHours(hours);
-	now.setMinutes(minutes);
-	now.setSeconds(seconds);
-
-	assert.equal(howLongTillLunch(...lunchtime), expected);
-	console.log(`\u001B[32m✓\u001B[39m ${expected}`);
-}
-
-let lunchtime = [ 12, 30 ];
-test(11, 30, 0, '1 hour');
-test(10, 30, 0, '2 hours');
-test(12, 25, 0, '5 minutes');
-test(12, 29, 15, '45 seconds');
-test(13, 30, 0, '23 hours');
-
-// some of us like an early lunch
-lunchtime = [ 11, 0 ];
-test(10, 30, 0, '30 minutes');
+test();
