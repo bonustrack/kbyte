ab4e468e56f54d876fa2cd75c84bad051cf9a3e4
adcpm <fabien@bonustrack.co>
2017-10-26 00:03:05 +0700

    Update tests

--- test/test.js
+++ test/test.js
@@ -4,34 +4,52 @@ const kbyte = require('..');
 const client = new kbyte.Client('wss://byteball.org/bb');
 
 const test = async () => {
-  /** Get Witnesses */
+  /** Get witnesses */
   const witnesses = await client.send('get_witnesses', null);
   console.log('Witnesses', witnesses);
 
-  /** Get Parents */
+  /** Get parents */
   const parents = await client.send('light/get_parents_and_last_ball_and_witness_list_unit', {
     witnesses,
   });
   console.log('Parents', parents);
 
-  /** Get Peers */
+  /** Get peers */
   const peers = await client.send('get_peers', null);
   console.log('Peers', peers);
 
-  /** Get History */
+  /** Get history */
   const history = await client.send('light/get_history', {
     witnesses,
     requested_joints: [parents.last_stable_mc_ball_unit],
+    // addresses: [],
   });
   console.log('History', history);
 
-  /** Get Joint */
+  /** Get joint */
   const joint = await client.send('get_joint', parents.last_stable_mc_ball_unit);
   console.log('Joint', joint);
 
-  /** Get Bots */
+  /** Get bots */
   const bots = await client.send('hub/get_bots', null);
   console.log('Bots', bots);
+
+  /** Get last MCI */
+  const mci = await client.send('get_last_mci', null);
+  console.log('Last MCI', mci);
+
+  /** Subscribe */
+  await client.send('subscribe', {
+    subscription_id: '1',
+    last_mci: mci,
+  });
+
+  /** Get hash tree */
+  const hashTree = await client.send('get_hash_tree', {
+    from_ball: joint.joint.unit.last_ball,
+    to_ball: parents.last_stable_mc_ball,
+  });
+  console.log('Hash tree', hashTree);
 };
 
 test();
