7ceb23930bd180a963bac98b8ef550666ef9d52f
adcpm <fabien@bonustrack.co>
2018-06-22 05:01:13 +0700

    Init utils

---
+++ dist/utils.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const Mnemonic = require('bitcore-mnemonic');
+
+const generateRandomSeed = () => {
+  let mnemonic = new Mnemonic();
+  while (!Mnemonic.isValid(mnemonic.toString())) {
+    mnemonic = new Mnemonic();
+  }
+  return mnemonic.phrase;
+};
+
+const isSeedValid = seed =>
+  Mnemonic.isValid(seed);
+
+var utils = {
+  generateRandomSeed,
+  isSeedValid,
+};
+
+module.exports = utils;
